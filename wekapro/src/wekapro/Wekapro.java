e53cffc62a1e2602d491f985e8febe36a0815d8a
ADMIN <ADMIN@DESKTOP-S5AJG5G>
2021-03-05 21:53:45 +0700

    lập trình tạo train - test

--- wekapro/src/wekapro/Wekapro.java
+++ wekapro/src/wekapro/Wekapro.java
@@ -28,12 +28,19 @@ public class Wekapro {
 //                "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
 //                "-R 2-3 ");
 //    model.mineAssociationRules();
-     MyFPGrowthModel model =new MyFPGrowthModel(
-              "C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
-              "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
-              "-N -R first-last");
-      model.mineAssociationRules(); 
+//     MyFPGrowthModel model =new MyFPGrowthModel(
+//              "C:\\Program Files\\Weka-3-8-5\\data\\weather.nominal.arff",
+//              "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.2",
+//              "-N -R first-last");
+//      model.mineAssociationRules(); 
+//     System.out.println(model);
+     MyKnowledgeModel model=new MyKnowledgeModel(
+     "C:\\Program Files\\Weka-3-8-5\\data\\iris.arff" ,null, null );
+     model.trainset= model.divideTraintestR(model.dataset, 80, false);
+     model.testset = model.divideTraintestR(model.dataset, 80, true);
      System.out.println(model);
+     System.out.println(model.trainset.toSummaryString());
+     System.out.println(model.testset.toSummaryString());
     }
     
 }
